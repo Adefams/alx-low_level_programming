0b0f8c3f25ecef3c81784bd8c6de47d4fd5a8282
adefams <aaadefams@gmail.com>
2022-09-19 17:03:30 -0700

    task4

---
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void print_rev(char *);
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+int main(void)
+{
+    char *str;
+
+    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+    print_rev(str);
+    return (0);
+}
